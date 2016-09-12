27fae1c29a6e8282a965a099b09d40421dab1474
PCliffordSF <paulclifford2570@gmail.com>
2016-09-12 06:21:47 -0700

    update checkbox code

--- js/main.js
+++ js/main.js
@@ -87,76 +87,96 @@ var createTotal = function(htmlString, totalCost) {
 	}
 } 
 
+// global variable of activities array
 var arrayOfActivities = $.makeArray($('.activities label'));
 
-// global variables
-	var tuesdayAM;
-	var wednesdayAM;
-	var tuesdayPM;
-	var wednesdayPM;
-
-
-
-
 checkboxes.change(function(){
-	var timeArray = [
-		[/Tuesday/, /Wednesday/],
-		[/9am-12pm/, /1pm-4pm/]
-	]
-	var textString = new RegExp($(this).parent()['0'].innerHTML);
+	// I hate this solution, but using data sourced from an innerHTML makes solving for the general case enormously complex. 
+	if ($(this)['0'].name === 'js-libs' && $(this)['0'].checked) {
+		arrayOfActivities[4].children['0'].disabled = true;
+	} else if ($(this)['0'].name === 'js-libs' && !$(this)['0'].checked) {
+		arrayOfActivities[4].children['0'].disabled = false;
+	
+	} else if ($(this)['0'].name === 'node' && $(this)['0'].checked) {
+		arrayOfActivities[2].children['0'].disabled = true;
+	} else if ($(this)['0'].name === 'node' && !$(this)['0'].checked) {
+		arrayOfActivities[2].children['0'].disabled = false;
+	
+	} else if ($(this)['0'].name === 'js-frameworks' && $(this)['0'].checked) {
+		arrayOfActivities[3].children['0'].disabled = true;
+	} else if ($(this)['0'].name === 'js-frameworks' && !$(this)['0'].checked) {
+		arrayOfActivities[3].children['0'].disabled = false;
+	
 
+	} else if ($(this)['0'].name === 'express' && $(this)['0'].checked) {
+		arrayOfActivities[1].children['0'].disabled = true;
+	} else if ($(this)['0'].name === 'express' && !$(this)['0'].checked) {
+		arrayOfActivities[1].children['0'].disabled = false;
+	}
 
-
-
-
-	setTime(timeArray, textString);
-
-	var thisName = ($(this)['0'].name);
-	console.log(thisName);
-
-
-	arrayOfActivities.forEach(function(activity){
-
-		// need to set the text string for the RegExp
-		
-		var textString2 = new RegExp(activity.innerHTML);
-
-		var arrayName = activity.children['0'].name;
-		if (getTime(timeArray, textString2) === "tuesdayAM" && tuesdayAM === true && thisName !== arrayName) {
-			activity.children['0'].disabled = true;
-		}
-		if (getTime(timeArray, textString2) === "tuesdayPM" && tuesdayPM === true && thisName !== arrayName) {
-			activity.children['0'].disabled = true;
-		}
-
-	});
 });
 
+$("fieldset:last div p")['0'].classList = "is-hidden"
+
+console.log($("fieldset:last div")['4']);
+console.log($("fieldset:last div")['5']);
 
 
 
 
 
 
-// sets the boolean time value for later testing;
-var setTime = function(timeArray, textString) {
-	for (var i = 0; i < timeArray.length; i++) {
-		for (var j = 0; j < timeArray.length; j++){
-			if (timeArray[0][0].test(textString) && timeArray[1][0].test(textString)) {tuesdayAM = true;}
-			if (timeArray[0][1].test(textString) && timeArray[1][0].test(textString)) {wednesdayAM = true;}
-			if (timeArray[0][0].test(textString) && timeArray[1][1].test(textString)) {tuesdayPM = true;}
-			if (timeArray[0][1].test(textString) && timeArray[1][1].test(textString)) {wednesdayPM = true;}
-		}
-	}
-}
+// checkboxes.change(function(){
+// 	var timeArray = [
+// 		[/Tuesday/, /Wednesday/],.addClass(".is-hidden")
+// 		[/9am-12pm/, /1pm-4pm/]
+// 	]
+// 	var textString = new RegExp($(this).parent()['0'].innerHTML);
 
-var getTime = function(timeArray, textString){
-	for (var i = 0; i < timeArray.length; i++) {
-		for (var j = 0; j < timeArray.length; j++) {
-			if (timeArray[0][0].test(textString) && timeArray[1][0].test(textString)) return "tuesdayAM";
-			if (timeArray[0][1].test(textString) && timeArray[1][0].test(textString)) return "wednesdayAM";
-			if (timeArray[0][0].test(textString) && timeArray[1][1].test(textString)) return "tuesdayPM";
-			if (timeArray[0][1].test(textString) && timeArray[1][1].test(textString)) return "wednesdayPM";
-		}
-	}
-}
+
+// 	setTime(timeArray, textString);
+
+// 	var thisName = ($(this)['0'].name);
+// 	console.log(thisName);
+
+
+// 	arrayOfActivities.forEach(function(activity){
+
+// 		// need to set the text string for the RegExp
+		
+// 		var textString2 = new RegExp(activity.innerHTML);
+
+// 		var arrayName = activity.children['0'].name;
+// 		if (getTime(timeArray, textString2) === "tuesdayAM" && tuesdayAM === true && thisName !== arrayName) {
+// 			activity.children['0'].disabled = true;
+// 		}
+// 		if (getTime(timeArray, textString2) === "tuesdayPM" && tuesdayPM === true && thisName !== arrayName) {
+// 			activity.children['0'].disabled = true;
+// 		}
+
+// 	});
+// });
+
+
+// // sets the boolean time value for later testing;
+// var setTime = function(timeArray, textString) {
+// 	for (var i = 0; i < timeArray.length; i++) {
+// 		for (var j = 0; j < timeArray.length; j++){
+// 			if (timeArray[0][0].test(textString) && timeArray[1][0].test(textString)) {tuesdayAM = true;}
+// 			if (timeArray[0][1].test(textString) && timeArray[1][0].test(textString)) {wednesdayAM = true;}
+// 			if (timeArray[0][0].test(textString) && timeArray[1][1].test(textString)) {tuesdayPM = true;}
+// 			if (timeArray[0][1].test(textString) && timeArray[1][1].test(textString)) {wednesdayPM = true;}
+// 		}
+// 	}
+// }
+
+// var getTime = function(timeArray, textString){
+// 	for (var i = 0; i < timeArray.length; i++) {
+// 		for (var j = 0; j < timeArray.length; j++) {
+// 			if (timeArray[0][0].test(textString) && timeArray[1][0].test(textString)) return "tuesdayAM";
+// 			if (timeArray[0][1].test(textString) && timeArray[1][0].test(textString)) return "wednesdayAM";
+// 			if (timeArray[0][0].test(textString) && timeArray[1][1].test(textString)) return "tuesdayPM";
+// 			if (timeArray[0][1].test(textString) && timeArray[1][1].test(textString)) return "wednesdayPM";
+// 		}
+// 	}
+// }
